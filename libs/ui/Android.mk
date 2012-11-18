8fb95edfabb0986264b1a713ea2ef1031e3cb2ec
erikcas <erikcas1972@gmail.com>
2012-11-19 00:20:29 +0100

    For samsung legacies build overlay to use GB camlibs. Need to ifdef this, for later

--- libs/ui/Android.mk
+++ libs/ui/Android.mk
@@ -16,6 +16,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES:= \
+        Overlay.cpp \
 	FramebufferNativeWindow.cpp \
 	GraphicBuffer.cpp \
 	GraphicBufferAllocator.cpp \
